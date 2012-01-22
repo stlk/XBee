c2421b0c9a6e5d1f431546b5073de2de2d572a53
stlk <josef.rousek@gmail.com>
2012-01-22 16:36:02 +0100

    added examples

---
+++ Examples/Example.cs
@@ -0,0 +1,74 @@
+using System;
+using Microsoft.SPOT;
+
+namespace STLK
+{
+    class Example
+    {
+        private static XBee _xBee;
+        private static XBeeButton _button;
+
+        public void Initialize()
+        {
+            _button = new XBeeButton(1);
+            _button.ButtonHandler += _button_ButtonHandler;
+
+            _xBee = new XBee();
+            _xBee.StatusReceived += _button.StatusReceived;
+        }
+
+        /// <summary>
+        /// Example of synchronous command with 500ms timeout
+        /// </summary>
+        /// <returns></returns>
+        public int GetRssiOfLastReceivedFrame()
+        {
+            ApiFrame rssiFrame = _xBee.SendRemoteAtCommandSync(XBeeCommand.ReceivedSignalStrength);
+
+            if (rssiFrame != null && rssiFrame.FrameData.Length == 16)
+            {
+                return rssiFrame.FrameData[15];
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        /// <summary>
+        /// Example of asynchronous command
+        /// </summary>
+        public void GetRssiOfLastReceivedFrameAsync()
+        {
+            _xBee.SendRemoteAtCommand(XBeeCommand.ReceivedSignalStrength, (frame) =>
+            {
+                if (frame.FrameData.Length != 16)
+                {
+                    return;
+                }
+
+                int rssi = frame.FrameData[15];
+                // do something with received value
+
+            });
+        }
+
+        /// <summary>
+        /// Example of Button class
+        /// </summary>
+        /// <param name="buttonHeld"></param>
+        /// <param name="ports"></param>
+        void _button_ButtonHandler(bool buttonHeld, byte ports)
+        {
+            if (buttonHeld)
+            {
+
+            }
+            else
+            {
+                // read state of output port and negate it
+                _xBee.SetDigitalOutput(XBeeCommand.ADio2Configuration, (((ports >> 2) & 1) == 0));
+            }
+        }
+    }
+}
